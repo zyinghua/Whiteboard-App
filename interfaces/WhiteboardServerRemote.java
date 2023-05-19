62a63c12881cad84bebe513a6e71a62de9a41c01
zyinghua <yzho0149@student.monash.edu>
2023-05-19 23:51:50 +1000

    method signatures for userlist, chat list, board image in bytes

--- interfaces/WhiteboardServerRemote.java
+++ interfaces/WhiteboardServerRemote.java
@@ -3,10 +3,16 @@
 
 package interfaces;
 
+import javax.swing.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface WhiteboardServerRemote extends Remote {
     boolean checkNameValidity(String username) throws RemoteException;
     boolean joinWhiteboard(String username) throws RemoteException;
+    DefaultListModel<String> getCurrUserListModel() throws RemoteException;
+    DefaultListModel<String> getChatListModel() throws RemoteException;
+    byte[] getWhiteboardImageInBytes() throws IOException;
 }
