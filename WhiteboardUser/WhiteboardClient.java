a98862d1acd7d66a4c59da4e84374acf0b4af74d
zyinghua <yzho0149@student.monash.edu>
2023-05-19 23:48:54 +1000

    implemented constructor. ready for join client

--- WhiteboardUser/WhiteboardClient.java
+++ WhiteboardUser/WhiteboardClient.java
@@ -1,8 +1,14 @@
 package WhiteboardUser;
 
-public class WhiteboardClient extends WhiteboardUser{
-    public WhiteboardClient(String username){
-        super(false, username);
-    }
+import javax.swing.*;
+import java.awt.image.BufferedImage;
 
+public class WhiteboardClient extends WhiteboardUser{
+
+    public WhiteboardClient(String username, DefaultListModel<String> currUserListModel, DefaultListModel<String> chatListModel, BufferedImage boardImage){
+        super(false, username);
+        this.setCurrUserListModel(currUserListModel);
+        this.setChatListModel(chatListModel);
+        this.setBoardImage(boardImage);
+    }
 }
