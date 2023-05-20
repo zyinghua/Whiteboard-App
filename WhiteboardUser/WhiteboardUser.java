2c90bce248bbbad35110eeac6ee1f769586f174a
zyinghua <yzho0149@student.monash.edu>
2023-05-20 18:52:52 +1000

    added send to remote method when send chat message, refactored associated methods

--- WhiteboardUser/WhiteboardUser.java
+++ WhiteboardUser/WhiteboardUser.java
@@ -2,9 +2,13 @@
 // Student ID: 1308266
 
 package WhiteboardUser;
+import remotes.WhiteboardUserRemote;
+
 import javax.swing.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.rmi.RemoteException;
+import java.util.HashMap;
 
 public class WhiteboardUser {
     private boolean isManager;
@@ -13,6 +17,7 @@ public class WhiteboardUser {
     private BoardPanel boardPanel;
     private DefaultListModel<String> chatListModel;
     private DefaultListModel<String> currUserListModel;
+    private HashMap<String, WhiteboardUserRemote> clientRemotes;
 
     public WhiteboardUser(boolean isManager, String username) {
         this.isManager = isManager;
@@ -21,6 +26,7 @@ public class WhiteboardUser {
         this.boardPanel = new BoardPanel();
         this.chatListModel = new DefaultListModel<>();
         this.currUserListModel = new DefaultListModel<>();
+        this.clientRemotes = new HashMap<>();
     }
 
     public boolean isManager() {
@@ -47,12 +53,13 @@ public class WhiteboardUser {
         this.chatListModel = chatListModel;
     }
 
-    public void addChatMessage(String username, String message) {
+    public void receiveChatMessage(String username, String message) {
         this.chatListModel.addElement(username + ": " + message);
     }
 
-    public void addChatMessage(String message) {
+    public void sendChatMessage(String message) {
         this.chatListModel.addElement(this.username + "(You) : " + message);
+        sendChatMessageRemote(message);
     }
 
     public DefaultListModel<String> getCurrUserListModel() {
@@ -81,5 +88,36 @@ public class WhiteboardUser {
 
     public void setUsername(String username) {
         this.username = username;
+    }
+
+    public HashMap<String, WhiteboardUserRemote> getClientRemotes() {
+        return clientRemotes;
+    }
+
+    public void setClientRemotes(HashMap<String, WhiteboardUserRemote> clientRemotes) {
+        this.clientRemotes = clientRemotes;
+    }
+    public void addClientRemote(String username, WhiteboardUserRemote clientRemote){
+        clientRemotes.put(username, clientRemote);
+    }
+
+    public void sendChatMessageRemote(String message) {
+        for (String username : clientRemotes.keySet()) {
+            if (!username.equals(this.username)) {
+                new Thread(() -> {
+                    try {
+                        clientRemotes.get(username).addChatMessage(this.username, message);
+                    } catch (RemoteException e) {
+                        if (isManager) {
+                            JOptionPane.showMessageDialog(null, "Something wrong with the remote connection to " + username + ". User removed.", "Error", JOptionPane.ERROR_MESSAGE);
+                            // kick the user
+
+                        } else {
+                            JOptionPane.showMessageDialog(null, "Something wrong with the remote connection, suggesting restart the program.", "Error", JOptionPane.ERROR_MESSAGE);
+                        }
+                    }
+                }).start();
+            }
+        }
     }
 }
