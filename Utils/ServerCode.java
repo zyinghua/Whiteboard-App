39ee08b0299795dab0725c1289b2ff0ca0f291ab
zyinghua <yzho0149@student.monash.edu>
2023-05-22 13:41:52 +1000

    added code to servercode

--- Utils/ServerCode.java
+++ Utils/ServerCode.java
@@ -4,5 +4,6 @@ public class ServerCode {
     public static final int JOIN_ACCEPTED = 0;
     public static final int JOIN_DENIED_USERNAME_ALREADY_EXISTS = 1;
     public static final int JOIN_DENIED_BY_MANAGER = 2;
-
+    public static final int JOIN_CANCELLED_DUE_TO_MANAGER_ERROR = 3;
+    public static final int JOIN_CANCELLED_BY_CLIENT = 4;
 }
